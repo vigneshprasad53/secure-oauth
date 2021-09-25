b1e361399470f710b9a6012e68d230baf871929b
vigneshprasad53 <81922826+vigneshprasad53@users.noreply.github.com>
2021-09-26 02:47:16 +0530

    [Commit - 1] Initial Commit

---
+++ app/app.js
@@ -0,0 +1,51 @@
+/**
+ * app.js
+ *
+ * This is the entry file for the application, only setup and boilerplate
+ * code.
+ */
+
+// Needed for redux-saga es6 generator support
+import '@babel/polyfill';
+
+// Import all the third party stuff
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router';
+import history from 'utils/history';
+import 'sanitize.css/sanitize.css';
+
+// Import root app
+import App from 'containers/App';
+
+
+import configureStore from './configureStore';
+
+
+// Create redux store with history
+const initialState = {};
+const store = configureStore(initialState, history);
+const MOUNT_NODE = document.getElementById('app');
+
+const render = () => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <App />
+      </ConnectedRouter>
+    </Provider>,
+    MOUNT_NODE,
+  );
+};
+
+if (module.hot) {
+  module.hot.accept(['containers/App'], () => {
+    ReactDOM.unmountComponentAtNode(MOUNT_NODE);
+    render();
+  });
+}
+
+render();
+
+
---
+++ app/containers/App/index.js
@@ -0,0 +1,19 @@
+/**
+ *
+ * App.js
+ *
+ * This component is the skeleton around the actual pages, and should only
+ * contain code that should be seen on all pages. (e.g. navigation bar)
+ *
+ */
+
+import React from 'react';
+
+
+export default function App() {
+  return (
+    <div>
+      Test
+    </div>
+  );
+}
